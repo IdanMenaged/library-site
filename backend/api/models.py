503ad42bae2b5d29f3166e22ec57a615709f5b4e
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2022-10-08 22:44:18 +0300

    api done

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+class Tag(models.Model):
+    text = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, primary_key=True)
+    author = models.CharField(max_length=100)
+    published = models.DateField()
+    cover = models.ImageField(upload_to='res/covers')
+    summary = models.TextField()
+    publisher = models.CharField(max_length=200)
+    count = models.IntegerField()
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+
+from .serializers import *
+from .models import *
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
 # Create your views here.
