0d3e1c17948dbb0479681487befaeb85a3950f33
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2022-10-08 23:44:44 +0300

    header

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './header.css'
+
+function Header(props) {
+    return (
+        <div className="header">
+            <h1>The Library of Alexendria 2.0</h1>
+            <h3>Hopefully it can't burn this time !</h3>
+        </div>
+    );
+}
+
+export default Header;
